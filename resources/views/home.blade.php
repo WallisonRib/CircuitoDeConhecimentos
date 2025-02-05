a7edda3ed0b45b9e1b0d5b8cf7c5f2d76704e121
WallisonRib <ribeirosantoswallison@gmail.com>
2025-02-05 16:16:15 -0300

    feat: adding home pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,12 @@
+@section('title', 'Circuito de Conhecimentos - Unimontes')
+
+
 <x-guest-layout>
- 
-   
-      
+<div class="">
+    <span>
+        teste
+    </span>
+</div>
+
 </x-guest-layout>
    
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="shortcut icon" href="{{ asset(path: 'images/circuito-logo-with-text.png') }}" type="image/x-icon">
+    
+    <title>@yield('title')</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
