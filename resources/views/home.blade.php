4035b689062a0376ebee2a7cb488dffe1deb551f
WallisonRib <ribeirosantoswallison@gmail.com>
2025-02-06 16:43:00 -0300

    feat: adding new layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,42 @@
-@section('title', 'Circuito de Conhecimentos - Unimontes')
+@extends('layouts.main')
+
+@section ('title', 'Circuito de Conhecimentos' )
+
+@section('header')
+
+@endsection
+
+@section('content')
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ad eos, itaque vitae dignissimos commodi odit corrupti numquam eius debitis saepe distinctio, odio perspiciatis dolorum est nihil at qui alias.</p>
+@endsection
 
 
-<x-guest-layout>
-<div class="">
-    <span>
-        teste
-    </span>
-</div>
 
-</x-guest-layout>
-   
+
+{{-- ESTRUTURA PARA LISTAGEM DE POSTS (AGUARDANDO CRIAÇÃO DE CONTROLLERS) --}}
+   {{-- <div class="p-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-between bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h2 class="font-semibold text-xl mb-4">Posts Recentes</h2>
+                    @foreach ($posts as $post)
+                        <div class="p-4 mb-4 rounded-lg shadow-sm">
+                            <h3 class="font-semibold text-lg">{{ $post->title }}</h3>
+                            <p>{{ $post->content }}</p>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h2 class="font-semibold text-xl mb-4">Posts Recentes</h2>
+                    @foreach ($posts as $post)
+                        <div class="p-4 mb-4 rounded-lg shadow-sm">
+                            <h3 class="font-semibold text-lg">{{ $post->title }}</h3>
+                            <p>{{ $post->content }}</p>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>--}}
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,30 +1,40 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="{{ asset(path: 'images/circuito-logo-with-text.png') }}" type="image/x-icon">
-    
-    <title>@yield('title')</title>
 
+    <title>@yield('title')</title>
+    
+    <link rel="shortcut icon" href="{{ asset('images/circuito-logo-with-text.png') }}" type="image/png">
+
+
+    <img  class="h-16 object-contain" alt="Descrição da imagem">
+    
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(entrypoints: ['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased">
+<body class="font-sans antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <!-- Page Heading -->
+        @include('components.navbar')
 
+        @yield('header')
+        <!-- Page Content -->
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-            {{ $slot }}
+            @yield('content')
         </div>
+
     </div>
 </body>
 
 </html>
+
